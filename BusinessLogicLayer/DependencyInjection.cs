476ee5cb20a110ffb40e82322a96d59557a04900
wickerknox <wickerknox89@gmail.com>
2025-01-30 15:56:36 -0800

    Add project files.

---
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.OrderMicroservice.BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessAccessLayer(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //TO DO: Add business logic layer services into the IoC container
+
+
+            return services;
+        }
+    }
+}
+
+
