13341a14f187961a8e78e6f6cb32cd41e97e55ec
wickerknox <wickerknox89@gmail.com>
2025-02-03 14:43:09 -0800

    Adding MongoDB

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace eCommerce.OrderMicroservice.DataAccessLayer
 {
@@ -9,13 +10,22 @@ namespace eCommerce.OrderMicroservice.DataAccessLayer
             IConfiguration configuration)
         {
             //TO DO: Add data access layer services into the IoC container
+            string connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+            string connectionString = connectionStringTemplate
+              .Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST"))
+              .Replace("$MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
 
+            services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+            services.AddScoped<IMongoDatabase>(provider =>
+            {
+                IMongoClient client = provider.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("OrdersDatabase");
+            });
 
             return services;
         }
     }
 }
-
-
 
 
