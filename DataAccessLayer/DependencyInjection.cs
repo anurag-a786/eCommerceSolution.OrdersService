513164b1fc9e41461ad70f2ef66914bb24074820
wickerknox <wickerknox89@gmail.com>
2025-02-04 11:12:42 -0800

    Orders Repository

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccessLayer.Repositories;
+using DataAccessLayer.RepositoryContracts;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
 
@@ -22,6 +24,8 @@ namespace eCommerce.OrderMicroservice.DataAccessLayer
                 IMongoClient client = provider.GetRequiredService<IMongoClient>();
                 return client.GetDatabase("OrdersDatabase");
             });
+
+            services.AddScoped<IOrdersRepository, OrdersRepository>();
 
             return services;
         }
---
+++ DataAccessLayer/RepositoryContracts/IOrdersRepository.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer.Entities;
+using MongoDB.Driver;
+
+namespace DataAccessLayer.RepositoryContracts
+{
+    public interface IOrdersRepository
+    {
+        /// <returns>Returns all orders from the orders collection</returns>
+        Task<IEnumerable<Order>> GetOrders();
+
+        /// <returns>Returning a collection of matching orders</returns>
+        Task<IEnumerable<Order?>> GetOrdersByCondition(FilterDefinition<Order> filter);
+
+        /// <returns>Returning matching order</returns>
+        Task<Order?> GetOrderByCondition(FilterDefinition<Order> filter);
+
+        /// <returns>Returns the added Order object or null if unsuccessful</returns>
+        Task<Order?> AddOrder(Order order);
+
+        /// <returns>Returns the updated order object; or null if not found</returns>
+        Task<Order?> UpdateOrder(Order order);
+
+        /// <returns>Returns true if the deletion is successful, false otherwise</returns>
+        Task<bool> DeleteOrder(Guid orderID);
+
+    }
+}
