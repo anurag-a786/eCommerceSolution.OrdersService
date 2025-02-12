4c62ef7aa2284fa2ffc6e823490367db053d65e5
wickerknox <wickerknox89@gmail.com>
2025-02-11 19:21:04 -0800

    Synchronous Microservice Communication using HttpClient : Loading Product Details in Orders Microservice

--- BusinessLogicLayer/DTO/OrderItemResponse.cs
+++ BusinessLogicLayer/DTO/OrderItemResponse.cs
@@ -1,9 +1,9 @@
 ﻿namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.DTO;
 
 public record OrderItemResponse(Guid ProductID, decimal UnitPrice, 
-    int Quantity, decimal TotalPrice)
+    int Quantity, decimal TotalPrice, string? ProductName, string? Category)
 {
-    public OrderItemResponse() : this(default, default, default, default)
+    public OrderItemResponse() : this(default, default, default, default, default, default)
     {
     }
 }
