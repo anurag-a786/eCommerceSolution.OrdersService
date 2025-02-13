5d8f6c05121b069d106b5c01156064f7d69003fc
wickerknox <wickerknox89@gmail.com>
2025-02-13 14:56:33 -0800

    Fault Tolerance : Exponential Backoff

--- BusinessLogicLayer/Policies/UsersMicroservicePolicies.cs
+++ BusinessLogicLayer/Policies/UsersMicroservicePolicies.cs
@@ -19,7 +19,7 @@ namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.Policies
                 Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
                 .WaitAndRetryAsync(
                      retryCount: 5, //Number of retries
-                     sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(2), // Delay between retries
+                     sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // Delay between retries
                      onRetry: (outcome, timespan, retryAttempt, context) =>
                      {
                          _logger.LogInformation($"Retry {retryAttempt} after {timespan.TotalSeconds} seconds");
