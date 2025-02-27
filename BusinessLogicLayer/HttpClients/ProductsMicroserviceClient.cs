8f7b66e308de394b45daf2837c903bd2419c2c3d
wickerknox <wickerknox89@gmail.com>
2025-02-26 19:40:40 -0800

    Caching : ServiceUnavailable Response

--- BusinessLogicLayer/HttpClients/ProductsMicroserviceClient.cs
+++ BusinessLogicLayer/HttpClients/ProductsMicroserviceClient.cs
@@ -40,7 +40,18 @@ namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.HttpClients
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    if (response.StatusCode == System.Net.HttpStatusCode.ServiceUnavailable)
+                    {
+                        ProductDTO? productFromFallback = await response.Content.ReadFromJsonAsync<ProductDTO>();
+
+                        if (productFromFallback == null)
+                        {
+                            throw new NotImplementedException("Fallback policy was not implemented");
+                        }
+
+                        return productFromFallback;
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
                     {
                         return null;
                     }
