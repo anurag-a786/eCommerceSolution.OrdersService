04d9769927aa72aa318ad9fa23386ecdaa2b18f0
wickerknox <wickerknox89@gmail.com>
2025-02-26 21:15:37 -0800

    Caching : Redis Cache for Users Microservice Data

--- BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
+++ BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
@@ -1,8 +1,10 @@
 ﻿using eCommerce.OrdersMicroservice.BusinessLogicLayer.DTO;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Polly.CircuitBreaker;
 using Polly.Timeout;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.HttpClients
 {
@@ -10,22 +12,46 @@ namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.HttpClients
     {
         private readonly HttpClient _httpClient;
         private readonly ILogger<UsersMicroserviceClient> _logger;
+        private readonly IDistributedCache _distributedCache;
 
-        public UsersMicroserviceClient(HttpClient httpClient, ILogger<UsersMicroserviceClient> logger)
+        public UsersMicroserviceClient(HttpClient httpClient, ILogger<UsersMicroserviceClient> logger, 
+            IDistributedCache distributedCache)
         {
             _httpClient = httpClient;
             _logger = logger;
+            _distributedCache = distributedCache;
         }
 
         public async Task<UserDTO?> GetUserByUserID(Guid userID)
         {
             try
             {
+                //Read the user data from cache
+                string cacheKeyToRead = $"user:{userID}";
+                string? cachedUser = await _distributedCache.GetStringAsync(cacheKeyToRead);
+                if (cachedUser != null)
+                {
+                    //Deserialized the cached user
+                    UserDTO? userFromCache = JsonSerializer.Deserialize<UserDTO>(cachedUser);
+                    return userFromCache;
+                }
+
                 HttpResponseMessage response = await _httpClient.GetAsync($"/api/users/{userID}");
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    if (response.StatusCode == System.Net.HttpStatusCode.ServiceUnavailable)
+                    {
+                        UserDTO? fallbackUser = await response.Content.ReadFromJsonAsync<UserDTO>();
+
+                        if (fallbackUser == null)
+                        {
+                            throw new NotImplementedException();
+                        }
+
+                        return fallbackUser;
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
                     {
                         return null;
                     }
@@ -51,6 +77,16 @@ namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.HttpClients
                 {
                     throw new ArgumentException("Invalid User ID");
                 }
+
+                //Store the user data (retrieved from response) into cache
+                string cacheKey = $"user:{userID}";
+                string userJson = JsonSerializer.Serialize(user);
+
+                DistributedCacheEntryOptions options = new DistributedCacheEntryOptions()
+                  .SetAbsoluteExpiration(DateTimeOffset.UtcNow.AddMinutes(5))
+                  .SetSlidingExpiration(TimeSpan.FromMinutes(3));
+
+                await _distributedCache.SetStringAsync(cacheKey, userJson, options);
 
                 return user;
             }
