b6a6b4719db17a2c11ca4a881713c1586967e774
wickerknox <wickerknox89@gmail.com>
2025-02-26 19:40:40 -0800

    Caching : ServiceUnavailable Response

--- BusinessLogicLayer/Policies/ProductsMicroservicePolicies.cs
+++ BusinessLogicLayer/Policies/ProductsMicroservicePolicies.cs
@@ -32,7 +32,7 @@ namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.Policies
                         QuantityInStock: 0
                         );
                     
-                    var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                    var response = new HttpResponseMessage(System.Net.HttpStatusCode.ServiceUnavailable)
                     {
                         Content = new StringContent(JsonSerializer.Serialize(product), Encoding.UTF8, "application/json")
                     };
