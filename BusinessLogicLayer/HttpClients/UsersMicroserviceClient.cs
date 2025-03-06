d059a2f36f7a1b65704f9659739b4ad3554635e5
wickerknox <wickerknox89@gmail.com>
2025-03-05 20:40:31 -0800

    API Gateway using Ocelot : Microservice Communication with API Gateway

--- BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
+++ BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
@@ -36,7 +36,7 @@ namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.HttpClients
                     return userFromCache;
                 }
 
-                HttpResponseMessage response = await _httpClient.GetAsync($"/api/users/{userID}");
+                HttpResponseMessage response = await _httpClient.GetAsync($"/gateway/users/{userID}");
 
                 if (!response.IsSuccessStatusCode)
                 {
